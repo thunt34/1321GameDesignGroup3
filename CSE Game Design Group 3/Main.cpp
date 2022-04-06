081bf1849190cfd8f50f49a028444d91fc87ebd0
thunt34 <99289839+thunt34@users.noreply.github.com>
2022-04-06 15:56:47 -0400

    Addition of bank teller UI

--- CSE Game Design Group 3/Main.cpp
+++ CSE Game Design Group 3/Main.cpp
@@ -1,20 +1,14 @@
-// CSE Game Design Group 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <string>
 #include <iostream>
+#include "bank.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int current_wallet = 300;
+    int account = 650;
+
+    bank b1(account, current_wallet);
+
+    b1.interact();
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
