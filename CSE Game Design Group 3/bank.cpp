00784d6aa9e9e33b723de20b50f86ceca9f48310
thunt34 <99289839+thunt34@users.noreply.github.com>
2022-04-06 19:00:47 -0400

    Create character object and data file

---
+++ CSE Game Design Group 3/bank.cpp
@@ -0,0 +1,92 @@
+#include "bank.h"
+#include <string>
+#include <iostream>
+
+bank::bank(short a, int b, int c) {
+	region = a;
+	savings = b;
+	wallet = c;
+}
+
+bank::bank(int a, int b) {
+	region = 0;
+	savings = a;
+	wallet = b;
+} // non-region specific overload for troubleshooting
+
+int bank::withdraw(int amt) {
+	savings -= amt;
+	wallet = wallet + amt;
+	return wallet;
+}
+
+int bank::deposit(int amt) {
+	savings += amt;
+	wallet = wallet - amt;
+	return wallet;
+}
+
+void bank::interact() {
+	short exit = 0;
+	short select;
+	int amt;
+	
+	std::cout << "Welcome to the bank! You currently have " << savings << " coins in your account!\n";
+	while (exit == 0) {
+		exit = 0;
+		std::cout << std::endl << "What would you like to do? \n"
+			<< "[1] Deposit\n" << "[2] Withdraw\n" << "[3] Exit\n";
+		std::cin >> select;
+		switch (select) {
+		case 1:
+			if (wallet <= 0) {
+				std::cout << "Sorry, you can not make a deposit at this time!\n";
+			}
+			else {
+				std::cout << "Okay, you currently have " << wallet << " coins in your wallet.\n"
+					<< "How much would you like to deposit? ";
+				std::cin >> amt;
+				if (amt > wallet) {
+					std::cout << "Sorry, you do not have that much in your wallet!\n";
+				}
+				else if (amt <= 0) {
+					std::cout << "Sorry, you have to put in a valid amount!\n";
+				}
+				else {
+					bank::deposit(amt);
+					std::cout << "You now have " << savings << " coins in your account.\n";
+				}
+			}
+			break;
+		case 2:
+			if (savings <= 0) {
+				std::cout << "Sorry, you can not make a withdrawal at this time!\n";
+			}
+			else {
+				std::cout << "Okay, you currently have " << savings << " coins in your account.\n"
+					<< "How much would you like to withdraw? ";
+				std::cin >> amt;
+				if (amt > savings) {
+					std::cout << "Sorry, you do not have that much in your account!\n";
+				}
+				else if (amt <= 0) {
+					std::cout << "Sorry, you have to withdraw a valid amount!\n";
+				}
+				else {
+					bank::withdraw(amt);
+					std::cout << "You now have " << savings << " coins in your account.\n";
+				}
+			}
+			break;
+		case 3:
+			std::cout << "Have a great day!\n";
+			exit = 1;
+			break;
+		default:
+			std::cout << "Sorry, I don't know how to help you with that!\n";
+		}
+	}
+
+	//include function to return new wallet and savings to player stats
+
+}
---
+++ CSE Game Design Group 3/character.cpp
@@ -0,0 +1,88 @@
+#include "character.h"
+#include <fstream>
+#include <string>
+
+character::character(short a, int b, int c) {
+	region = a;
+	wallet = b;
+	savings = c;
+}
+
+void character::create() {
+	std::ofstream outFile;
+	outFile.open("chardata.txt");
+	for (int i = 0; i < 10; i++) {
+		outFile << 0 << std::endl;
+	}
+}
+
+void character::load() {
+	std::string line;
+	std::ifstream inFile;
+	inFile.open("chardata.txt");
+
+	for (int i = 0; i < 10; i++) {
+		getline(inFile, line);
+		switch (i) {
+		case 0:
+			region = stoi(line);
+			break;
+		case 1:
+			wallet = stoi(line);
+			break;
+		case 2:
+			savings = stoi(line);
+			break;
+		}
+	}
+}
+
+void character::overwrite(short a, int b) {
+	std::string line;
+
+	std::ofstream outFile;
+	std::ifstream inFile;
+
+	short index = a;
+	int data = b;
+
+	inFile.open("chardata.txt");
+	outFile.open("hold.txt");
+
+	for (int i = 0; i < 10; i++) {
+		if (i != index) {
+			getline(inFile, line);
+			outFile << line << std::endl;
+		}
+		else if (i == index) {
+			outFile << data << std::endl;
+			getline(inFile, line);
+		}
+	}
+
+	inFile.close();
+	outFile.close();
+
+	inFile.open("hold.txt");
+	outFile.open("chardata.txt");
+
+	for (int i = 0; i < 10; i++) {
+		getline(inFile, line);
+		outFile << line << std::endl;
+	}
+
+	inFile.close();
+	outFile.close();
+}
+
+short character::getRegion() {
+	return region;
+}
+
+int character::getWallet() {
+	return wallet;
+}
+
+int character::getSavings() {
+	return savings;
+}
