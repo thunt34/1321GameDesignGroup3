fda77246dff9e181fbbb3ebf416dc4e59519ad46
Firestrike61 <102167199+Firestrike61@users.noreply.github.com>
2022-05-02 00:43:07 -0400

    Update combat.cpp

--- CSE Game Design Group 3/combat.cpp
+++ CSE Game Design Group 3/combat.cpp
@@ -13,38 +13,36 @@ combat::combat(enemy& newM) : M(newM) {
 
 void combat::combatChoice(character& C) {
 
-   C.display();
-  string choice;
-  do{
-    cout << "What do you do? (a)ttack, (h)eal, (f)lee" << endl;
-  
-      cin >> choice;
+    C.display();
+    string choice = "";
+    do {
+        cout << "What do you do? (a)ttack, (f)lee" << endl;
 
-    while(choice.size() != 1){
-      cout << "Please enter only one character." << endl << endl;
-      cin >> choice;
+        cin >> choice;
+
+        while (choice.size() != 1) {
+            cout << "Please enter only one character." << endl << endl;
+            cin >> choice;
+        }
+
+        for_each(choice.begin(), choice.end(), [](char& c) {
+            c = tolower(c);
+            });
+
+        if ((choice != "a") && (choice != "f")) {
+            cout << "Invalid input!" << endl << endl;
+        }
+
+    } while ((choice != "a") && (choice != "f"));
+
+    if (choice == "a") {
+        C.attack(M);
     }
 
-    for_each(choice.begin(), choice.end(), [](char & c) {
-         c = tolower(c);
-      });
-    
-    if((choice!="a")&&(choice!="h")&&(choice!="f")){
-      cout << "Invalid input!" << endl << endl;
+    else if (choice == "f") {
+        C.flee(C);
     }
-
-    }while((choice!="a")&&(choice!="h")&&(choice!="f"));
-  
-          if(choice == "a"){
-            C.attack(M);
-            }
-          else if(choice == "h"){
-            C.heal(C);
-    }
-
-          else if(choice == "f"){
-            C.flee(C);
-  }
+}
 
 void combat::combat1(character& C) {
 
@@ -55,6 +53,8 @@ void combat::combat1(character& C) {
     }
     if (M.mhealth > 0 && C.charHP > 0 && C.aflee == 1) {
         cout << C.name << " has fleed the battle!" << endl;
+        C.cflee = 0;
+        C.aflee = 0;
     }
 
     if (M.mhealth <= 0) {
