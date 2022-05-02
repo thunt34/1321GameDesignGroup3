6bf15f67d2b86162bc5961bcec93bf7985a69927
thunt34 <99289839+thunt34@users.noreply.github.com>
2022-05-02 01:52:14 -0400

    Added error catcher to bank

--- CSE Game Design Group 3/bank.cpp
+++ CSE Game Design Group 3/bank.cpp
@@ -1,8 +1,11 @@
 #include "bank.h"
+#include "catch_err.h"
 #include <string>
 #include <iostream>
 #include <chrono>
 #include <thread>
+
+catch_err b;
 
 bank::bank(double a, int b, int c) {
 	interest = a;
@@ -30,7 +33,7 @@ int bank::deposit(int amt) {
 
 void bank::interact() {
 	short exit = 0;
-	short select;
+	std::string select;
 	int amt;
 	bool madeDeposit = 0;
 	
@@ -41,58 +44,61 @@ void bank::interact() {
 		exit = 0;
 		std::cout << std::endl << "What would you like to do? \n"
 			<< "[1] Deposit\n" << "[2] Withdraw\n" << "[3] Exit\n";
-		std::cin >> select;
-		switch (select) {
-		case 1:
-			if (wallet <= 0) {
-				std::cout << "Sorry, you can not make a deposit at this time!\n";
-			}
-			else {
-				std::cout << "Okay, you currently have " << wallet << " coins in your wallet.\n"
-					<< "How much would you like to deposit? ";
-				std::cin >> amt;
-				if (amt > wallet) {
-					std::cout << "Sorry, you do not have that much in your wallet!\n";
-				}
-				else if (amt <= 0) {
-					std::cout << "Sorry, you have to put in a valid amount!\n";
+		
+		do {
+			std::cin >> select;
+			switch (b.getVal(select)) {
+			case 1:
+				if (wallet <= 0) {
+					std::cout << "Sorry, you can not make a deposit at this time!\n";
 				}
 				else {
-					bank::deposit(amt);
-					madeDeposit = 1;
-					std::cout << "You now have " << savings << " coins in your account.\n";
+					std::cout << "Okay, you currently have " << wallet << " coins in your wallet.\n"
+						<< "How much would you like to deposit? ";
+					std::cin >> amt;
+					if (amt > wallet) {
+						std::cout << "Sorry, you do not have that much in your wallet!\n";
+					}
+					else if (amt <= 0) {
+						std::cout << "Sorry, you have to put in a valid amount!\n";
+					}
+					else {
+						bank::deposit(amt);
+						madeDeposit = 1;
+						std::cout << "You now have " << savings << " coins in your account.\n";
+					}
 				}
-			}
-			break;
-		case 2:
-			if (savings <= 0) {
-				std::cout << "Sorry, you can not make a withdrawal at this time!\n";
-			}
-			else {
-				std::cout << "Okay, you currently have " << savings << " coins in your account.\n"
-					<< "How much would you like to withdraw? ";
-				std::cin >> amt;
-				if (amt > savings) {
-					std::cout << "Sorry, you do not have that much in your account!\n";
-				}
-				else if (amt <= 0) {
-					std::cout << "Sorry, you have to withdraw a valid amount!\n";
+				break;
+			case 2:
+				if (savings <= 0) {
+					std::cout << "Sorry, you can not make a withdrawal at this time!\n";
 				}
 				else {
-					bank::withdraw(amt);
-					std::cout << "You now have " << savings << " coins in your account.\n";
+					std::cout << "Okay, you currently have " << savings << " coins in your account.\n"
+						<< "How much would you like to withdraw? ";
+					std::cin >> amt;
+					if (amt > savings) {
+						std::cout << "Sorry, you do not have that much in your account!\n";
+					}
+					else if (amt <= 0) {
+						std::cout << "Sorry, you have to withdraw a valid amount!\n";
+					}
+					else {
+						bank::withdraw(amt);
+						std::cout << "You now have " << savings << " coins in your account.\n";
+					}
 				}
+				break;
+			case 3:
+				std::cout << "Have a great day!\n";
+				exit = 1;
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+				system("cls");
+				break;
+			default:
+				std::cout << "Sorry, I don't know how to help you with that!\n";
 			}
-			break;
-		case 3:
-			std::cout << "Have a great day!\n";
-			exit = 1;
-			std::this_thread::sleep_for(std::chrono::seconds(2));
-			system("cls");
-			break;
-		default:
-			std::cout << "Sorry, I don't know how to help you with that!\n";
-		}
+		} while (b.err(select) == 0);
 	}
 	if (madeDeposit == 1) {
 		interest = interest + 0.2;
